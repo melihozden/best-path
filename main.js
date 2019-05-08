87f9df6ec73ad73bb24e9cc36105586c07cbc1d3
melihozden <melihozden403@gmail.com>
2019-05-08 17:04:20 +0300

    p5js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+// Finding Best Top Left corner to Right Bottom corner...
+// Coding train inspired me to this. 
+
+
+function setup(params) {
+    createCanvas(400, 400);
+    console.log("A*")
+}
+function draw() {
+    background(0);
+}
+setup();
