a2769b1904b1904d2c00c2abf6f5e9efb94fe396
melihozden <melihozden403@gmail.com>
2019-05-08 23:42:55 +0300

    p5.js constructing

--- main.js
+++ main.js
@@ -2,13 +2,45 @@
 
 // Finding Best Top Left corner to Right Bottom corner...
 // Coding train inspired me to this. 
+var rows = 5;
+var cols = 5;
+var grid = new Array(cols);
 
+var openSet = []
+var closedSet = []
 
-function setup(params) {
+var startPoint;
+var endPoint
+
+// f,g and h values , f(n) = g(n) + h(n)
+function Node() {
+    this.f = 0
+    this.g = 0
+    this.h = 0
+}
+
+function setup() {
     createCanvas(400, 400);
     console.log("A*")
+
+    for (var i = 0; i < cols; i++) {
+        grid[i] = new Array(rows)
+    }
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j] = new Node();
+        }
+    }
+    startPoint = grid[0][0] // top-left corner
+    endPoint = grid[cols - 1][rows - 1] // bottom-right corner
+
+    openSet.push(startPoint);
+
+
+    console.log(grid)
+
 }
 function draw() {
     background(0);
 }
-setup();
