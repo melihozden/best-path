4c4cf72c741448c79e4c620a01b577661deb5769
melihozden <melihozden403@gmail.com>
2019-05-09 03:24:43 +0300

    coloring

--- main.js
+++ main.js
@@ -6,22 +6,38 @@ var rows = 5;
 var cols = 5;
 var grid = new Array(cols);
 
-var openSet = []
-var closedSet = []
+var openSet = [];
+var closedSet = [];
 
 var startPoint;
-var endPoint
+var endPoint;
+var w, h;
 
 // f,g and h values , f(n) = g(n) + h(n)
-function Node() {
-    this.f = 0
-    this.g = 0
-    this.h = 0
+function Node(i, j) {
+
+    this.x = i;
+    this.y = j;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+
+    this.show = function (color) {
+        fill(color);
+        noStroke();
+        rect(this.x * w, this.y * h, w - 1, h - 1)
+    }
+
 }
 
 function setup() {
-    createCanvas(400, 400);
+    width = 400;
+    height = 400;
+    createCanvas(width, height);
     console.log("A*")
+    w = width / cols;
+    h = height / rows;
+
 
     for (var i = 0; i < cols; i++) {
         grid[i] = new Array(rows)
@@ -29,7 +45,7 @@ function setup() {
 
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
-            grid[i][j] = new Node();
+            grid[i][j] = new Node(i, j);
         }
     }
     startPoint = grid[0][0] // top-left corner
@@ -37,10 +53,33 @@ function setup() {
 
     openSet.push(startPoint);
 
-
     console.log(grid)
+
 
 }
 function draw() {
+    if (openSet.length > 0) {
+        // keep going
+    }
+    else {
+        // done   
+    }
     background(0);
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show(color(255));
+        }
+    }
+    for (var i = 0; i < closedSet.length; i++) {
+        closedSet[i].show(color(255, 0, 0))
+    }
+
+    for (var i = 0; i < openSet.length; i++) {
+        openSet[i].show(color(0, 255, 0))
+
+    }
+
+
+
 }
